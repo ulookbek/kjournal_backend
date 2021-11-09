3ef7f0445d6248663fa9263cc61b7b03bd67fc91
Улукбек Капарбеков <ulukbekkaparbekov@gmail.com>
2021-11-10 03:19:15 +0600

    Create base entities schema

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,9 +1,31 @@
 import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
+import { TypeOrmModule } from '@nestjs/typeorm';
+import { UserModule } from './user/user.module';
+import { User as UserEntity } from './user/entities/user.entity';
+import { ArticleModule } from './article/article.module';
+import { Comment as CommentEntity} from './comment/entities/comment.entity';
+import { Article as ArticleEntity } from './article/entities/article.entity';
+import { CommentModule } from './comment/comment.module';
+
 
 @Module({
-  imports: [],
+  imports: [
+    TypeOrmModule.forRoot({
+      type: 'postgres',
+      host: 'localhost',
+      port: 5432,
+      username: 'postgres',
+      password: '123123',
+      database: 'kjournal',
+      entities: [UserEntity, ArticleEntity, CommentEntity],
+      synchronize: true,
+    }),
+    UserModule,
+    ArticleModule,
+    CommentModule,
+  ],
   controllers: [AppController],
   providers: [AppService],
 })
